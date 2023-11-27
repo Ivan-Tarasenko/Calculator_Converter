34de9837746c53607cc4e161e0fe98fd285d797c
Ivan-Tarasenko <80888936+Ivan-Tarasenko@users.noreply.github.com>
2023-11-27 22:40:20 +0900

    added locallization

--- Calculator/Screens/InfoScreen/Controller/InfoViewController.swift
+++ Calculator/Screens/InfoScreen/Controller/InfoViewController.swift
@@ -9,7 +9,23 @@ import UIKit
 
 final class InfoViewController: UIViewController {
     
+    @IBOutlet weak var descriptionAPILabel: UILabel!
+    @IBOutlet weak var appVersionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        localizationLabels()
+    }
+    
+    private func localizationLabels() {
+        descriptionAPILabel.txt = NSLocalizedString("descAPI", comment: "")
+    }
+    
+    func showAppVersion() {
+        if let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
+            appVersionLabel.txt = appVersion
+        } else {
+            appVersionLabel.txt = "1.0.1"
+        }
     }
 }
